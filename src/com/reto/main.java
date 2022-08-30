b69da5db60b55e67eb5d61b49303d749ca527c34
Mateo-olier <aolierguerrero@outlook.com>
2022-08-30 17:27:17 -0500

    agregando boolean

--- src/com/reto/main.java
+++ src/com/reto/main.java
@@ -6,12 +6,21 @@ public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        boolean mayor;
+
         System.out.println("Ingresa tu nombre: ");
         String name = sc.nextLine();
         System.out.println("Ingresar edad: ");
         int age = sc.nextInt();
         System.out.println("Ingresar estatura: ");
         double height = sc.nextDouble();
-        System.out.println("nombre: "+name +" edad: "+age+ " estatura: " + height);
+
+        if(age>17){
+            mayor = true;
+        }else{
+            mayor = false;
+        }
+
+        System.out.println("nombre: "+name +" edad: "+age+ " estatura: " + height + " es mayor: "+ mayor);
     }
 }
